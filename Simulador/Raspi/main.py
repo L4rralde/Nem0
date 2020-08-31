05143bc522569036baad73c32ad97007c1743727
L4rralde <33332433+L4rralde@users.noreply.github.com>
2020-08-31 14:50:38 -0500

    Create main.py

---
+++ Simulador/Raspi/main.py
@@ -0,0 +1,32 @@
+import flask 
+import time 
+from models import varFuncs
+from instrument import variable 
+
+lVariables = []
+for name, func in varFuncs: 
+	lVariables.append(variable(name, func))
+
+app = flask.Flask(__name__)
+t0 = 0
+
+@app.route('/')
+def inicio():
+	#return '<p>Bienvenido</p><p><a href="htpp://raspberrypi.local:5000/monitor">Monitor</a></p>'
+	return '<p>Bienvenido</p><p><a href="htpp://localhost:5000/monitor">Monitor</a></p>'
+@app.route('/monitor')
+def main():
+	global t0
+	if(t0==0):
+		t = 0
+		t0 = time.time()
+	else: 
+		t = time.time()-t0
+	paquete = []
+
+	for var in lVariables: 
+		paquete.append(var.empaquetar(t))
+	return flask.jsonify(paquete)
+
+if __name__=="__main__":
+	app.run(host='0.0.0.0', port=5000)
